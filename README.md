ef8126b73271e0914a79dbc955fa11004b5f9989
yucheng <2667389861@qq.com>
2023-06-03 21:05:52 +0800

    feat: 完成首次启动收集所有文件导出的变量，支持ts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project-snippet
+
+# config
+
+```js
+const options = {
+  dirs: ["src/**/*"], // 一个符合 fast-glob 匹配模式的数组，默认会匹配src下的所有文件
+};
+```
---
+++ test.js
@@ -0,0 +1,14 @@
+
+/**
+ * npm install --save-dev @babel/core
+ * const babel = require("@babel/core");
+
+babel.transformSync("code", optionsObject);
+ */
+
+/**
+ * npm install --save-dev @babel/plugin-transform-typescript
+ * {
+  "plugins": ["@babel/plugin-transform-typescript"]
+}
+ */
